112427ef030ee314480a8b526b5456cbfefb1ee7
anannanur <anannaanu974@gmail.com>
2022-03-26 17:59:17 +0600

    choose one product randomly functionality is added

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -6,7 +6,8 @@ import './Shop.css';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
-    
+    const [chosen_One, setChosen_One] = useState([]);
+
     useEffect(() => {
         fetch('data.json')
             .then(res => res.json())
@@ -14,8 +15,21 @@ const Shop = () => {
     }, [])
 
     const handleAddToCart = item => {
+
         const newCart = [...cart, item];
-        setCart(newCart);
+        if (cart.length < 4) {
+            setCart(newCart);
+        }
+        else {
+            alert("you can't choose more than 4");
+        }
+
+    }
+    const chooseOneId = () => {
+        const chooseOne = Math.round(Math.random(cart.length) * (cart.length-1));
+        console.log(cart.length-1);
+        console.log(chooseOne);
+        setChosen_One((cart[chooseOne].name));
     }
 
     return (
@@ -34,9 +48,10 @@ const Shop = () => {
                     {
                         cart.map(cart => <Cart cart={cart} key={cart.id}></Cart>)
                     }
-                    <button className='choose-btn1' >Choose One</button>
+                    <button className='choose-btn1' onClick={chooseOneId}>Choose One</button>
                     <button className='choose-btn2'>Choose again</button>
-                    
+                    <p>{chosen_One}</p>
+
                 </div>
             </div>
         </div>
